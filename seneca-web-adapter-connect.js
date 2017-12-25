52aad7a6cf147c761fa5eaceb7e0f881b74f631c
EamonnK <amen_cionna@hotmail.com>
2017-12-25 01:03:06 +0000

    Code quality control updated - .eslintrc.js added/prettier installed

--- seneca-web-adapter-connect.js
+++ seneca-web-adapter-connect.js
@@ -5,7 +5,7 @@ const QueryString = require('querystring')
 const URL = require('url')
 const ReadBody = require('./read-body')
 
-module.exports = function connect (options, context, auth, routes, done) {
+module.exports = function connect(options, context, auth, routes, done) {
   const seneca = this
 
   // middleware is an object with keys defining middleware
@@ -14,52 +14,63 @@ module.exports = function connect (options, context, auth, routes, done) {
     return done(new Error('no context provided'))
   }
 
-  _.each(routes, (route) => {
+  _.each(routes, route => {
     // pull out middleware from the route; map strings to options' middleware.
     // if we don't get a function, blow up hard - this is a user-code problem.
     const routeMiddleware = (route.middleware || []).map(_middleware => {
-      const ret = _.isString(_middleware) ? middleware[_middleware] : _middleware
+      const ret = _.isString(_middleware)
+        ? middleware[_middleware]
+        : _middleware
       if (!_.isFunction(ret)) {
         throw new Error(`expected valid middleware, got ${_middleware}`)
       }
       return ret
     })
 
-    context.use(route.path, composeMiddleware(routeMiddleware.concat([(request, reply, next) => {
-      // Connect does not work with http verbs
-      if (route.methods.indexOf(request.method) !== -1) {
-        // if parsing body, call into ReadBody otherwise just finish.
-        if (options.parseBody) { return ReadBody(request, finish) }
-        finish(null, request.body || {})
-      }
+    context.use(
+      route.path,
+      composeMiddleware(
+        routeMiddleware.concat([
+          (request, reply, next) => {
+            // Connect does not work with http verbs
+            if (route.methods.indexOf(request.method) !== -1) {
+              // if parsing body, call into ReadBody otherwise just finish.
+              if (options.parseBody) {
+                return ReadBody(request, finish)
+              }
+              finish(null, request.body || {})
+            }
 
-      function finish (err, body) {
-        if (err) {
-          return next(err)
-        }
+            function finish(err, body) {
+              if (err) {
+                return next(err)
+              }
 
-        var payload = {
-          request$: request,
-          response$: reply,
-          args: {
-            body: body,
-            route: route,
-            query: QueryString.parse(URL.parse(request.originalUrl).query)
+              var payload = {
+                request$: request,
+                response$: reply,
+                args: {
+                  body: body,
+                  route: route,
+                  query: QueryString.parse(URL.parse(request.originalUrl).query)
+                }
+              }
+              seneca.act(route.pattern, payload, (err, response) => {
+                if (err) {
+                  return next(err)
+                }
+                if (route.autoreply) {
+                  reply.writeHead(200, { 'Content-Type': 'application/json' })
+                  reply.end(JSON.stringify(response))
+                }
+              })
+            }
           }
-        }
-        seneca.act(route.pattern, payload, (err, response) => {
-          if (err) {
-            return next(err)
-          }
-          if (route.autoreply) {
-            reply.writeHead(200, {'Content-Type': 'application/json'})
-            reply.end(JSON.stringify(response))
-          }
-        })
-      }
-    }])))
+        ])
+      )
+    )
   })
-  return done(null, {routes: routes})
+  return done(null, { routes: routes })
 }
 
 /**
@@ -70,8 +81,10 @@ module.exports = function connect (options, context, auth, routes, done) {
 const composeMiddleware = _middleware => {
   return _middleware.reduce((previous, current) => {
     return (req, res, next) => {
-      previous(req, res, (err) => {
-        if (err) { return next(err) }
+      previous(req, res, err => {
+        if (err) {
+          return next(err)
+        }
         current(req, res, next)
       })
     }
--- test/connect.test.js
+++ test/connect.test.js
@@ -9,7 +9,7 @@ const Connect = require('connect')
 const BodyParser = require('body-parser')
 
 const expect = Code.expect
-const lab = exports.lab = Lab.script()
+const lab = (exports.lab = Lab.script())
 const describe = lab.describe
 const it = lab.it
 const beforeEach = lab.beforeEach
@@ -26,15 +26,18 @@ describe('connect', () => {
   let server = null
 
   const middleware = {
-    head: (req, res, next) => { res.writeHead(200, {'Content-Type': 'application/json'}); next() },
-    res: (req, res, next) => res.end(JSON.stringify({success: true}))
+    head: (req, res, next) => {
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      next()
+    },
+    res: (req, res) => res.end(JSON.stringify({ success: true }))
   }
 
   beforeEach(done => {
     app = Connect()
     server = app.listen(3000, () => {
-      si = Seneca({log: 'silent'})
-      si.use(Web, {adapter: require('..'), context: app, middleware})
+      si = Seneca({ log: 'silent' })
+      si.use(Web, { adapter: require('..'), context: app, middleware })
       si.ready(done)
     })
   })
@@ -43,7 +46,7 @@ describe('connect', () => {
     server.close(done)
   })
 
-  it('by default routes autoreply', (done) => {
+  it('by default routes autoreply', done => {
     var config = {
       routes: {
         pin: 'role:test,cmd:*',
@@ -54,22 +57,22 @@ describe('connect', () => {
     }
 
     si.add('role:test,cmd:ping', (msg, reply) => {
-      reply(null, {res: 'pong!'})
+      reply(null, { res: 'pong!' })
     })
 
-    si.act('role:web', config, (err, reply) => {
+    si.act('role:web', config, (err) => {
       if (err) return done(err)
 
       Request('http://127.0.0.1:3000/ping', (err, res, body) => {
         if (err) return done(err)
         body = JSON.parse(body)
-        expect(body).to.be.equal({res: 'pong!'})
+        expect(body).to.be.equal({ res: 'pong!' })
         done()
       })
     })
   })
 
-  it('multiple routes supported', (done) => {
+  it('multiple routes supported', done => {
     var config = {
       routes: {
         pin: 'role:test,cmd:*',
@@ -81,14 +84,14 @@ describe('connect', () => {
     }
 
     si.add('role:test,cmd:one', (msg, reply) => {
-      reply(null, {res: 'pong!'})
+      reply(null, { res: 'pong!' })
     })
 
     si.add('role:test,cmd:two', (msg, reply) => {
-      reply(null, {res: 'ping!'})
+      reply(null, { res: 'ping!' })
     })
 
-    si.act('role:web', config, (err, reply) => {
+    si.act('role:web', config, (err) => {
       if (err) return done(err)
 
       Request('http://127.0.0.1:3000/one', (err, res, body) => {
@@ -96,21 +99,21 @@ describe('connect', () => {
 
         body = JSON.parse(body)
 
-        expect(body).to.be.equal({res: 'pong!'})
+        expect(body).to.be.equal({ res: 'pong!' })
 
         Request('http://127.0.0.1:3000/two', (err, res, body) => {
           if (err) return done(err)
 
           body = JSON.parse(body)
 
-          expect(body).to.be.equal({res: 'ping!'})
+          expect(body).to.be.equal({ res: 'ping!' })
           done()
         })
       })
     })
   })
 
-  it('post without body parser defined', (done) => {
+  it('post without body parser defined', done => {
     var config = {
       routes: {
         pin: 'role:test,cmd:*',
@@ -123,21 +126,25 @@ describe('connect', () => {
     }
 
     si.add('role:test,cmd:echo', (msg, reply) => {
-      reply(null, {value: msg.args.body})
+      reply(null, { value: msg.args.body })
     })
 
-    si.act('role:web', config, (err, reply) => {
+    si.act('role:web', config, (err) => {
       if (err) return done(err)
 
-      Request.post('http://127.0.0.1:3000/echo', {json: {foo: 'bar'}}, (err, res, body) => {
-        if (err) return done(err)
-        expect(body.value).to.be.equal('{"foo":"bar"}')
-        done()
-      })
+      Request.post(
+        'http://127.0.0.1:3000/echo',
+        { json: { foo: 'bar' } },
+        (err, res, body) => {
+          if (err) return done(err)
+          expect(body.value).to.be.equal('{"foo":"bar"}')
+          done()
+        }
+      )
     })
   })
 
-  it('post with body parser defined', (done) => {
+  it('post with body parser defined', done => {
     var config = {
       options: {
         parseBody: false
@@ -158,18 +165,22 @@ describe('connect', () => {
       reply(null, msg.args.body)
     })
 
-    si.act('role:web', config, (err, reply) => {
+    si.act('role:web', config, (err) => {
       if (err) return done(err)
 
-      Request.post('http://127.0.0.1:3000/echo', {json: {foo: 'bar'}}, (err, res, body) => {
-        if (err) return done(err)
-        expect(body).to.be.equal({foo: 'bar'})
-        done()
-      })
+      Request.post(
+        'http://127.0.0.1:3000/echo',
+        { json: { foo: 'bar' } },
+        (err, res, body) => {
+          if (err) return done(err)
+          expect(body).to.be.equal({ foo: 'bar' })
+          done()
+        }
+      )
     })
   })
 
-  it('can handle custom errors properly', (done) => {
+  it('can handle custom errors properly', done => {
     var config = {
       routes: {
         pin: 'role:test,cmd:*',
@@ -183,20 +194,26 @@ describe('connect', () => {
       reply(new Error('aw snap!'))
     })
 
-    si.act('role:web', config, (err, reply) => {
+    si.act('role:web', config, (err) => {
       if (err) return done(err)
 
       app.use((err, req, res, next) => {
-        if (res.headersSent) { return next(err) }
-        res.writeHead(400, {'Content-Type': 'application/json'})
-        return res.end(JSON.stringify({message: err.orig.message.replace('gate-executor: ', '')}))
+        if (res.headersSent) {
+          return next(err)
+        }
+        res.writeHead(400, { 'Content-Type': 'application/json' })
+        return res.end(
+          JSON.stringify({
+            message: err.orig.message.replace('gate-executor: ', '')
+          })
+        )
       })
 
       Request('http://127.0.0.1:3000/boom', (err, res, body) => {
         if (err) return done(err)
         body = JSON.parse(body)
         expect(res.statusCode).to.equal(400)
-        expect(body).to.be.equal({message: 'aw snap!'})
+        expect(body).to.be.equal({ message: 'aw snap!' })
         done()
       })
     })
@@ -213,8 +230,10 @@ describe('connect', () => {
           }
         }
       }
-      si.act('role:web', config, (err, reply) => {
-        expect(err.details.message).to.equal('expected valid middleware, got total not valid')
+      si.act('role:web', config, (err) => {
+        expect(err.details.message).to.equal(
+          'expected valid middleware, got total not valid'
+        )
         done()
       })
     })
@@ -230,16 +249,16 @@ describe('connect', () => {
       }
 
       si.add('role:test,cmd:ping', (msg, reply) => {
-        reply(null, {res: 'ping!'})
+        reply(null, { res: 'ping!' })
       })
 
-      si.act('role:web', config, (err, reply) => {
+      si.act('role:web', config, (err) => {
         if (err) return done(err)
         Request('http://127.0.0.1:3000/ping', (err, res, body) => {
           if (err) return done(err)
           body = JSON.parse(body)
           expect(res.statusCode).to.equal(200)
-          expect(body).to.be.equal({success: true})
+          expect(body).to.be.equal({ success: true })
           done()
         })
       })
@@ -255,24 +274,27 @@ describe('connect', () => {
       }
 
       si.add('role:test,cmd:ping', (msg, reply) => {
-        reply(null, {res: 'ping!'})
+        reply(null, { res: 'ping!' })
       })
 
-      si.add('role:web,routes:*', function (msg, cb) {
+      si.add('role:web,routes:*', function(msg, cb) {
         msg.routes.middleware = [
-          (req, res, next) => { res.writeHead(200, {'Content-Type': 'application/json'}); next() },
-          (req, res, next) => res.end(JSON.stringify({success: true}))
+          (req, res, next) => {
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            next()
+          },
+          (req, res) => res.end(JSON.stringify({ success: true }))
         ]
         this.prior(msg, cb)
       })
 
-      si.act('role:web', config, (err, reply) => {
+      si.act('role:web', config, (err) => {
         if (err) return done(err)
         Request('http://127.0.0.1:3000/ping', (err, res, body) => {
           if (err) return done(err)
           body = JSON.parse(body)
           expect(res.statusCode).to.equal(200)
-          expect(body).to.be.equal({success: true})
+          expect(body).to.be.equal({ success: true })
           done()
         })
       })
